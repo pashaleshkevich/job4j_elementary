1f626fcc8af7aa33e813826cdb709be31f5958c5
Sumyork <psychohistorian.p@gmail.com>
2025-07-10 11:18:17 +0300

    4.2. Результат работы метода [#232622 #476551] - practice

---
+++ src/main/java/ru/job4j/MathFunction.java
@@ -0,0 +1,20 @@
+package ru.job4j;
+
+public class MathFunction {
+
+    public static int func1(int x) {
+        return x * x + 1;
+    }
+
+    public static int func2(int x) {
+        return 1 / x;
+    }
+
+    public static void main(String[] args) {
+        int result1 = MathFunction.func1(3);
+        int result2 = MathFunction.func2(5);
+        int result3 = MathFunction.func2(100);
+        int total = result1 + result2 + result3;
+        System.out.println(total);
+    }
+}
