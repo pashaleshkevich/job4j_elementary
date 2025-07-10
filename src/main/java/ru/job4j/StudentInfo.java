57b32ced80cdbc743daba7a022aed5230d3938de
Sumyork <psychohistorian.p@gmail.com>
2025-07-10 09:11:22 +0300

    4.1. Разбор первой программы. [#159033 #476378] - theory

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Hi, my name is Pasha Leshkevich.");
+        System.out.println("10.07.2025");
+    }
+}
