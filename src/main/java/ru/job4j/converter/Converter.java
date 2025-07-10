15651c59d81d818a0bb1f15178289f676396c385
Sumyork <psychohistorian.p@gmail.com>
2025-07-10 15:30:02 +0300

    1. Модульные тесты. [#159435 #476724] - practice

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -11,17 +11,4 @@ public class Converter {
         float result = value / 60;
         return result;
     }
-
-    public static void main(String[] args) {
-        float input = 140;
-        float expected = 2;
-        float output = Converter.rubleToEuro(input);
-        boolean passed = expected == output;
-        System.out.println("140 rubles are 2. Test result : " + passed);
-        input = 180;
-        expected = 3;
-        output = Converter.rubleToDollar(input);
-        passed = expected == output;
-        System.out.println("180 rubles are 3. Test result : " + passed);
-    }
 }
