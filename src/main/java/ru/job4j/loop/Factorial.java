362c3d05256800bdf3cb74f49437f470b7c51577
Pasha Leshkevich <sum.infinite.jest@gmail.com>
2025-07-14 11:22:20 +0300

    5.2. Создать программу, вычисляющую факториал. [#193 #476991] - practice

---
+++ src/main/java/ru/job4j/loop/Factorial.java
@@ -0,0 +1,12 @@
+package ru.job4j.loop;
+
+public class Factorial {
+
+    public static int calculate(int number) {
+        int result = 1;
+        for (int i = 2; i <= number; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
