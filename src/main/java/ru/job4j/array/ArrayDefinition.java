a96da1fd4714c8b491a73a6805d9227af93a6e3e
Pasha Leshkevich <sum.infinite.jest@gmail.com>
2025-07-15 16:13:23 +0300

    6.0.3. Заполнение массива. [#226300 #477076] - practice

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -2,11 +2,14 @@ package ru.job4j.array;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
-        short[] ages = new short[3];
-        String[] surname = new String[100500];
-        float[] prices = new float[40];
-        System.out.println(ages.length);
-        System.out.println(surname.length);
-        System.out.println(prices.length);
+        String[] names = new String[4];
+        names[0] = "Patrick Mahomes";
+        names[1] = "Joe Burrow";
+        names[2] = "Aaron Rodgers";
+        names[3] = "Josh Allen";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
