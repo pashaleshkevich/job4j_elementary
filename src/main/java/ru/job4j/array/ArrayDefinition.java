b64fc99b03a2ae0e444c0d9046691e7f23a43b9e
Pasha Leshkevich <sum.infinite.jest@gmail.com>
2025-07-15 16:30:01 +0300

    6.0.4. Массивы и цикл for. [#226665 #477078] - practice

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -7,9 +7,8 @@ public class ArrayDefinition {
         names[1] = "Joe Burrow";
         names[2] = "Aaron Rodgers";
         names[3] = "Josh Allen";
-        System.out.println(names[0]);
-        System.out.println(names[1]);
-        System.out.println(names[2]);
-        System.out.println(names[3]);
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
     }
 }
